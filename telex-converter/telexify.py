ae7fd59c239f34a50dd983f15fd48cc8688caefe
ammon134 <ammon134@gmail.com>
2023-12-12 11:14:44 +0700

    fix: add output folder creation to telexify

--- telex-converter/telexify.py
+++ telex-converter/telexify.py
@@ -6,19 +6,24 @@ from pathlib import Path
 
 # Maximum number of output file
 OUTPUT_COUNT_LIMIT = 1
-OUTPUT_PATH = "data/output/"
+OUTPUT_PATH = Path("data/output/batch2").resolve()
 
 
 def main():
-
     input_file, number_of_lines = validate_usage()
 
-    telex_mapping = load_telex_mapping("/Users/quang-dang/Documents/viet-corpus-converter/telex-converter/new_telex_mapping.json")
+    telex_mapping = load_telex_mapping(
+        "/Users/quang-dang/Documents/viet-corpus-converter/telex-converter/new_telex_mapping-w.json"
+    )
     if not validate_telex_mapping(telex_mapping):
-        sys.exit('telex_mapping contains invalid info')
-    
-    output_files = convert_multiple_lines_to_telex(input_file, telex_mapping, number_of_lines)
-    print(f"{len(output_files)} files created successfully. Output written to {OUTPUT_PATH}.")
+        sys.exit("telex_mapping contains invalid info")
+
+    output_files = convert_multiple_lines_to_telex(
+        input_file, telex_mapping, number_of_lines
+    )
+    print(
+        f"{len(output_files)} files created successfully. Output written to {OUTPUT_PATH}."
+    )
 
 
 def validate_usage() -> tuple:
@@ -38,51 +43,49 @@ def load_telex_mapping(input_file: str) -> dict:
         sys.exit("Mapping file not found, loading failed.")
 
 
-
 def validate_telex_mapping(mapping: dict) -> bool:
-    mapping = mapping['mapping']
+    mapping = mapping["mapping"]
     for item in mapping:
-        if item['mod'] not in ['a', 'e', 'o', 'w', 'd', '']:
+        if item["mod"] not in ["a", "e", "o", "w", "d", ""]:
             print(f"Error with {item['name']}")
             return False
-        if item['diacritics'] not in ['s', 'f', 'r', 'x', 'j', '']:
+        if item["diacritics"] not in ["s", "f", "r", "x", "j", ""]:
             print(f"Error with {item['name']}")
             return False
-        if len(item['name']) == 2 and item['name'][-1] != item['main'][-1]:
-            if item['main'] == 'uo':
+        if len(item["name"]) == 2 and item["name"][-1] != item["main"][-1]:
+            if item["main"] == "uo" or item["main"] == "wo":
                 pass
             else:
                 print(f"Error with {item['name']}")
                 return False
-            
+
     return True
-        
+
 
 def convert_word_to_telex(word: str, telex_mapping_json: dict) -> str:
-    telex_word = ''
+    telex_word = ""
     i = 0
     word_len = len(word)
     try:
-        telex_mapping = telex_mapping_json['mapping']
+        telex_mapping = telex_mapping_json["mapping"]
     except IndexError:
-        sys.exit('Mapping does not exist.')
+        sys.exit("Mapping does not exist.")
 
     while i < word_len:
         found = False
         for j in range(min(5, word_len - i), 0, -1):
-            subword = word[i:i+j]
+            subword = word[i : i + j]
             for vowel in telex_mapping:
-                if subword == vowel['name']:
-                    
-                    telex_word += vowel['main'] + vowel['mod'] + vowel['diacritics']
+                if subword == vowel["name"]:
+                    telex_word += vowel["main"] + vowel["mod"] + vowel["diacritics"]
                     i += j
                     found = True
                     break
-        
+
         if not found:
             telex_word += word[i]
             i += 1
-    
+
     return telex_word
 
 
@@ -92,12 +95,14 @@ def convert_line_to_telex(line: str, telex_mapping: dict) -> str:
     return " ".join(telex_line)
 
 
-def convert_multiple_lines_to_telex(input_file: str, telex_mapping: dict, number_of_lines: int) -> int:
+def convert_multiple_lines_to_telex(
+    input_file: str, telex_mapping: dict, number_of_lines: int
+) -> int:
     try:
         with open(input_file, encoding="utf-8") as f:
             output_files = []
             telex_lines = []
-            
+
             for line in f:
                 telex_line = convert_line_to_telex(line, telex_mapping)
                 telex_lines.append(telex_line)
@@ -108,7 +113,7 @@ def convert_multiple_lines_to_telex(input_file: str, telex_mapping: dict, number
 
                 if len(output_files) == OUTPUT_COUNT_LIMIT:
                     break
-        
+
         return output_files
 
     except FileNotFoundError:
@@ -122,9 +127,14 @@ def save_telex_output(telex_lines: list, output_files: list) -> list:
     output_count = len(output_files) + 1
 
     output_name = f"telex_output_{today}_{str(output_count)}.txt"
-    output_file = Path(OUTPUT_PATH) / output_name
-    with open(output_file, 'w', encoding="utf-8") as f_out:
-        f_out.write("\n".join(telex_lines))
+    output_file = OUTPUT_PATH / output_name
+
+    try:
+        with open(output_file, "w", encoding="utf-8") as f_out:
+            print("File opened.")
+            f_out.write("\n".join(telex_lines))
+    except BaseException as err:
+        sys.exit(f"Failed to save file. {err}")
     print(f"Created {output_file}.")
     output_files.append(output_file)
     return output_files
